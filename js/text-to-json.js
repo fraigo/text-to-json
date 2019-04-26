d65ff913dc82be9ac77955201f31f2ef61397515
Francisco <franciscoigor@gmail.com>
2019-04-26 11:38:09 -0700

    Updated CSV2JSON

--- js/text-to-json.js
+++ js/text-to-json.js
@@ -59,14 +59,116 @@ function readBlob(file,callback) {
 }
 
 
+function CSVToArray( strData, strDelimiter ){
+	if (strData.trim()==""){
+		return [];
+	}
+	var lines=strData.split("\n");
+	var sep=getSeparator(lines[0]);
+	// Check to see if the delimiter is defined. If not,
+	// then default to comma.
+	strDelimiter = (strDelimiter || sep);
+
+	// Create a regular expression to parse the CSV values.
+	var objPattern = new RegExp(
+		(
+			// Delimiters.
+			"(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+			// Quoted fields.
+			"(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+			// Standard fields.
+			"([^\"\\" + strDelimiter + "\\r\\n]*))"
+		),
+		"gi"
+		);
+
+
+	// Create an array to hold our data. Give the array
+	// a default empty first row.
+	var arrData = [[]];
+
+	// Create an array to hold our individual pattern
+	// matching groups.
+	var arrMatches = null;
+
+
+	// Keep looping over the regular expression matches
+	// until we can no longer find a match.
+	while (arrMatches = objPattern.exec( strData )){
+
+		// Get the delimiter that was found.
+		var strMatchedDelimiter = arrMatches[ 1 ];
+
+		// Check to see if the given delimiter has a length
+		// (is not the start of string) and if it matches
+		// field delimiter. If id does not, then we know
+		// that this delimiter is a row delimiter.
+		if (
+			strMatchedDelimiter.length &&
+			strMatchedDelimiter !== strDelimiter
+			){
+
+			// Since we have reached a new row of data,
+			// add an empty row to our data array.
+			arrData.push( [] );
+
+		}
+
+		var strMatchedValue;
+
+		// Now that we have our delimiter out of the way,
+		// let's check to see which kind of value we
+		// captured (quoted or unquoted).
+		if (arrMatches[ 2 ]){
+
+			// We found a quoted value. When we capture
+			// this value, unescape any double quotes.
+			strMatchedValue = arrMatches[ 2 ].replace(
+				new RegExp( "\"\"", "g" ),
+				"\""
+				);
+
+		} else {
+
+			// We found a non-quoted value.
+			strMatchedValue = arrMatches[ 3 ];
+
+		}
+
+
+		// Now that we have our value string, let's add
+		// it to the data array.
+		arrData[ arrData.length - 1 ].push( strMatchedValue );
+	}
+
+	// Return the parsed data.
+	return( arrData );
+}
+
+
 function textToArray(string){
+	
     var lines=string.split("\n");
 	var sep=getSeparator(lines[0]);
 	var result=[];
     for(var index in lines){
         lines[index]=lines[index].trim();
 		var cols=lines[index].split(sep);
-        result.push(cols);
+		console.log("COLS",cols);
+		if (cols.length>0){
+			if (cols[0]==""){
+				console.log("Append",cols);
+				for(var col in cols){
+					if (cols[col]!=""){
+						result[result.length-1][col]+="\n"+cols[col];
+					}
+				}
+			}else{
+				result.push(cols);
+			}
+		}
     }
 	return result;
 }
